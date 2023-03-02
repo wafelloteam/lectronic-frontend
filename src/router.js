f4d9dc9e244fa7182b9af0c51954b8e2a6528b4a
AnggaArdhinata <ardhinataangga@gmail.com>
2023-03-02 20:18:05 +0700

    init project

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
+
+function router() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="" element='' />
+        <Route path="" element='' />
+        <Route path="" element='' />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default router
